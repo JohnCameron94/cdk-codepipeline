404146d6835dc7148111fb398002d7eedae74ad0
JohnCameron94 <ec2-user@ip-172-31-10-157.ca-central-1.compute.internal>
2022-08-30 01:00:22 +0000

    gitignore and environment

--- bin/entry-point.ts
+++ bin/entry-point.ts
@@ -2,12 +2,13 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PipelineStack } from '../lib/pipeline-stack';
+import { dev } from '../environment';
 
 const app = new cdk.App();
 new PipelineStack(app, 'PipelineStack', {
   env: {
-    account:'john-aws-playground',
-    region: 'ca-central-1'
+    account:dev.account,
+    region: dev.region
   }
 });
 

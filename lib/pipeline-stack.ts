fc04b74d66a0eaaf44e79b0950fabdb553acbf07
JohnCameron94 <ec2-user@ip-172-31-10-157.ca-central-1.compute.internal>
2022-08-30 02:40:04 +0000

    modifying way to connect to github since Cloudformation has a Defect

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -16,8 +16,13 @@ export class PipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth',{
-        input: CodePipelineSource.gitHub(
-          'JohnCameron94/cdk-codepipeline','main',{authentication: cdk.SecretValue.secretsManager('GitHubToken')}),
+        input: CodePipelineSource.connection(
+          'JohnCameron94/cdk-codepipeline',
+          'main',
+          {
+            connectionArn: 'arn:aws:codestar-connections:ca-central-1:360070888501:connection/5199c46f-94d5-4428-b063-9513fed3c5e2'
+          }
+        ),
           commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });

5a8e40381d8d19a3db0833330b84818a571f21bf
JohnCameron94 <ec2-user@ip-172-31-10-157.ca-central-1.compute.internal>
2022-08-30 01:38:07 +0000

    test

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -17,7 +17,7 @@ export class PipelineStack extends cdk.Stack {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth',{
         input: CodePipelineSource.gitHub(
-          'JohnCameron94/cdk-codepipeline','main',{authentication: cdk.SecretValue.secretsManager('GithubToken')}),
+          'JohnCameron94/cdk-codepipeline','main',{authentication: cdk.SecretValue.secretsManager('GitHubToken')}),
           commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
